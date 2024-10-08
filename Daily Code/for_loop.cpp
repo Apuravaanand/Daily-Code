b5c23c20aa886e2222f61b8b9ecc12d169793ffa
@purava__ <115463013+Apuravaanand@users.noreply.github.com>
2024-10-08 22:48:29 +0530

    Add files via upload

--- Daily Code/for_loop.cpp
+++ Daily Code/for_loop.cpp
@@ -1,13 +1,13 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int n,i,j;
-    cout<<"Give the value of n";
+    int n;
+    cout<<"Give the value of n"<<endl;
     cin>>n;
 
     for(int i=1;i<=n;i++){
         for(int j=1;j<=n;j++){
-            cout<<i;
+            cout<<j;
         }
         cout<<endl;
     }
